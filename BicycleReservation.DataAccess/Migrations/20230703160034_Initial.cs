9aa7d6c6e17d6c287ea45a589167b6a2caf85892
Ensar <ensarhamzic01@gmail.com>
2023-07-03 18:00:59 +0200

    nove migracije

--- BicycleReservation.DataAccess/Migrations/20230703160034_Initial.cs
+++ BicycleReservation.DataAccess/Migrations/20230703160034_Initial.cs
@@ -16,6 +16,7 @@ namespace BicycleReservation.DataAccess.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     LockCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
@@ -101,6 +102,11 @@ namespace BicycleReservation.DataAccess.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "ForgotPasswordToken", "ImageUrl", "LastName", "PasswordHash", "PasswordSalt", "Role", "Username", "VerificationToken" },
+                values: new object[] { -1, "admin@admin.com", "Admin", null, null, "Admin", new byte[] { 99, 19, 107, 216, 197, 138, 178, 64, 165, 59, 124, 68, 198, 221, 199, 126, 70, 67, 214, 232, 105, 31, 174, 154, 36, 89, 65, 213, 248, 127, 195, 178, 168, 230, 4, 18, 12, 128, 176, 83, 98, 209, 108, 167, 232, 118, 239, 200, 71, 243, 220, 193, 233, 171, 38, 126, 105, 155, 136, 72, 244, 30, 196, 237 }, new byte[] { 64, 108, 169, 92, 83, 161, 66, 200, 92, 157, 250, 49, 45, 210, 72, 57, 142, 168, 188, 39, 17, 54, 23, 215, 183, 133, 206, 113, 11, 222, 183, 232, 63, 35, 167, 153, 24, 112, 227, 173, 199, 217, 103, 55, 252, 117, 75, 234, 220, 70, 98, 167, 161, 227, 28, 113, 124, 125, 66, 68, 127, 91, 248, 85, 95, 241, 248, 48, 132, 35, 234, 148, 46, 7, 86, 197, 10, 44, 7, 115, 53, 6, 40, 147, 216, 129, 97, 155, 206, 149, 6, 20, 153, 158, 53, 69, 195, 29, 153, 70, 185, 54, 229, 248, 202, 254, 188, 15, 80, 210, 217, 85, 50, 236, 20, 98, 40, 119, 102, 233, 206, 240, 61, 130, 14, 131, 223, 113 }, 3, "admin", null });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Records_BicycleId",
